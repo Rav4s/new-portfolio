15f6a89d13164e2602171105b8bbc15e3575d36e
Rav4s <ravidhruvishshah@gmail.com>
2022-08-12 12:06:05 -0500

    OMG THE LINKS WORK

---
+++ js/redirects.js
@@ -0,0 +1,24 @@
+document.getElementById("github").onclick = function() {
+    location.href = 'https://github.com/Rav4s'
+};
+document.getElementById("linkedin").onclick = function() {
+    location.href = 'https://www.linkedin.com/in/ravishah2824/'
+};
+document.getElementById("reddit").onclick = function() {
+    location.href = 'https://www.reddit.com/user/rav4s'
+};
+document.getElementById("youtube").onclick = function() {
+    location.href = 'https://www.youtube.com/c/yeetpc/'
+};
+document.getElementById("twitter").onclick = function() {
+    location.href = 'https://twitter.com/rav4ss'
+};
+document.getElementById("spotify").onclick = function() {
+    location.href = 'https://open.spotify.com/user/31phgvwpvtnltp2d5rpjp5obaoiu?si=24193fbe8b2d4f73'
+};
+document.getElementById("flickr").onclick = function() {
+    location.href = 'https://www.flickr.com/photos/195790143@N02/'
+};
+document.getElementById("pinterest").onclick = function() {
+    location.href = 'https://www.pinterest.com/rav4ss/'
+};
