f754f8b4559eed65f47ac35164155a61d8f9874c
Ravi Shah <ravidhruvishshah@gmail.com>
2024-08-21 23:40:50 -0500

    update coursework and icons

--- js/redirects.js
+++ js/redirects.js
@@ -4,21 +4,6 @@ document.getElementById("github").onclick = function() {
 document.getElementById("linkedin").onclick = function() {
     location.href = 'https://www.linkedin.com/in/ravishah2824/'
 };
-document.getElementById("reddit").onclick = function() {
-    location.href = 'https://www.reddit.com/user/rav4s'
-};
-document.getElementById("youtube").onclick = function() {
-    location.href = 'https://www.youtube.com/c/yeetpc/'
-};
-document.getElementById("twitter").onclick = function() {
-    location.href = 'https://twitter.com/rav4ss'
-};
-document.getElementById("spotify").onclick = function() {
-    location.href = 'https://open.spotify.com/user/31phgvwpvtnltp2d5rpjp5obaoiu?si=24193fbe8b2d4f73'
-};
-document.getElementById("flickr").onclick = function() {
-    location.href = 'https://www.flickr.com/photos/195790143@N02/'
-};
-document.getElementById("pinterest").onclick = function() {
-    location.href = 'https://www.pinterest.com/rav4ss/'
+document.getElementById("mail").onclick = function() {
+    location.href = 'mailto:ravi.shah@utexas.edu'
 };
