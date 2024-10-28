2276169f6aa627371ec30d7dc2a648273423bedd
Ravi Shah <ravidhruvishshah@gmail.com>
2024-10-28 18:39:52 -0500

    finish first project entry

---
+++ js/per_project/kline.js
@@ -0,0 +1,12 @@
+document.getElementById("project-github").onclick = function() {
+    location.href = 'https://github.com/Rav4s/diy-shift-indicator'
+};
+document.getElementById("project-writeup").onclick = function() {
+    location.href = 'https://drive.google.com/file/d/1h4sEP5iF2pkmRt12-wAFUMqSv9QvsOz3/view'
+};
+document.getElementById("project-blog").onclick = function() {
+    location.href = 'https://blog.yeetpc.com/k-line-iso-9141-shift-indicator/'
+};
+document.getElementById("project-video").onclick = function() {
+    location.href = 'https://drive.google.com/file/d/1uUzjxnDBYHzpRwuf0LmYo11RxKfqu849/view'
+};
