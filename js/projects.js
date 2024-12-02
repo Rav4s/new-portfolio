667379421afced968a617d89509e37571267c46b
Ravi Shah <ravidhruvishshah@gmail.com>
2024-12-02 01:01:15 -0600

    add placeholders for 3 more projects

--- js/projects.js
+++ js/projects.js
@@ -1,3 +1,12 @@
 document.getElementById("kline").onclick = function() {
     location.href = './k-line-sequential-shift-indicator/'
 };
+document.getElementById("civic").onclick = function() {
+    location.href = './1998-honda-civic-rebuild/'
+};
+document.getElementById("rocket").onclick = function() {
+    location.href = './2024-tarc-rocket-clown-car/'
+};
+document.getElementById("environment").onclick = function() {
+    location.href = './environmental-monitoring-telemetry-system/'
+};
