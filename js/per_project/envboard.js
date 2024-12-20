c0e861fa294bd2b138c94693a878599d335b1346
Ravi Shah <ravidhruvishshah@gmail.com>
2024-12-20 18:53:33 +0530

    add project stuff

---
+++ js/per_project/envboard.js
@@ -0,0 +1,6 @@
+document.getElementById("project-github").onclick = function() {
+    location.href = 'https://github.com/lhr-solar/DataAcq-EnvironmentBoard'
+};
+document.getElementById("project-writeup").onclick = function() {
+    location.href = 'https://cloud.wikis.utexas.edu/wiki/spaces/LHRSOLAR/pages/205226271/Data+Acquisition+Environment+Board'
+};
