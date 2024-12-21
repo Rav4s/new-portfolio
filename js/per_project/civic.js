b4a2fb64582a7fdb853ba55d6cfa59a88b15c0de
Ravi Shah <ravidhruvishshah@gmail.com>
2024-12-21 10:41:15 +0530

    add mroe projects'

---
+++ js/per_project/civic.js
@@ -0,0 +1,12 @@
+document.getElementById("project-blog").onclick = function() {
+    location.href = 'https://blog.yeetpc.com/rebuilding-a-salvage-1998-honda-civic/'
+};
+document.getElementById("project-blog-2").onclick = function() {
+    location.href = 'https://blog.yeetpc.com/overhauling-my-1998-honda-civic-interior/'
+};
+document.getElementById("project-blog-3").onclick = function() {
+    location.href = 'https://blog.yeetpc.com/brakes-brakes-brakes/'
+};
+document.getElementById("project-blog-4").onclick = function() {
+    location.href = 'https://blog.yeetpc.com/replacing-a-broken-lug-stud-on-my-1998-civic-project/'
+};
